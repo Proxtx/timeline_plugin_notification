da8232ad3c1df8cba7596be81004b3ec80919dca
Proxtx <gopro54122@gmail.com>
2024-03-24 19:38:04 +0100

    working

---
+++ client.rs
@@ -0,0 +1,46 @@
+use {
+    leptos::{view, View, IntoView},
+    serde::Deserialize
+};
+
+pub struct Plugin {
+    
+}
+
+impl crate::plugin_manager::Plugin for Plugin {
+    fn get_style(&self) -> crate::plugin_manager::Style {
+        crate::plugin_manager::Style::Acc2
+    }
+
+    async fn new(_data: crate::plugin_manager::PluginData) -> Self
+        where
+            Self: Sized {
+        Plugin {}
+    }
+
+    fn get_component(&self, data: crate::plugin_manager::PluginEventData) -> crate::event_manager::EventResult<Box<dyn FnOnce() -> leptos::View>> {
+        let data= data.get_data::<Notification>()?;
+        Ok(Box::new(
+            move || -> View {
+                view! {
+                    <div style="display: flex; flex-direction: row; width: 100%; gap: calc(var(--contentSpacing) * 0.5); background-color: var(--accentColor2);align-items: start;">
+                    <img style="width: calc(var(--contentSpacing) * 5); aspect-ratio: 1;" src=move || {crate::api::relative_url(&format!("/api/plugin/timeline_plugin_notification/icon/{}.ico", data.app)).unwrap().to_string()}/>
+                    <div style="padding-top: calc(var(--contentSpacing) * 0.5); padding-bottom: calc(var(--contentSpacing) * 0.5); color: var(--lightColor);">
+                    <h3>{
+                        move || {data.title.clone()}
+                    }</h3>
+                    <a>{move || {data.content.clone()}}</a>
+                    </div>
+                    </div>
+                }.into_view()
+            }
+        ))
+    }
+}
+
+#[derive(Deserialize)]
+struct Notification {
+    app: String,
+    title: String,
+    content: String,
+}
---
+++ server.rs
@@ -0,0 +1,124 @@
+use {
+    crate::{
+        config::Config,
+        db::{Database, Event},
+        PluginData,
+    },
+    chrono::Utc,
+    futures::StreamExt,
+    rocket::{
+        http::{Status},
+        get, routes,
+        serde::json::Json,
+        State,
+        fs::NamedFile
+    },
+    serde::{Deserialize, Serialize},
+    std::{path::PathBuf, sync::Arc},
+    types::{
+        api::{APIResult, APIError, AvailablePlugins, CompressedEvent},
+        timing::Timing,
+    },
+};
+
+pub struct Plugin {
+    plugin_data: PluginData,
+}
+
+impl crate::Plugin for Plugin {
+    async fn new(data: crate::PluginData) -> Self
+    where
+        Self: Sized,
+    {
+        Plugin { plugin_data: data }
+    }
+
+    fn get_type() -> types::api::AvailablePlugins
+    where
+        Self: Sized,
+    {
+        types::api::AvailablePlugins::timeline_plugin_notification
+    }
+
+    fn get_routes() -> Vec<rocket::Route>
+    where
+        Self: Sized,
+    {
+        routes![new_notification, app_icon]
+    }
+
+    fn get_compressed_events(
+        &self,
+        query_range: &types::timing::TimeRange,
+    ) -> std::pin::Pin<
+        Box<
+            dyn futures::Future<Output = types::api::APIResult<Vec<types::api::CompressedEvent>>>
+                + Send,
+        >,
+    > {
+        let filter = Database::generate_range_filter(query_range);
+        let database = self.plugin_data.database.clone();
+        Box::pin(async move {
+            let mut cursor = database
+                .get_events::<Notification>()
+                .find(filter, None)
+                .await?;
+            let mut result = Vec::new();
+            while let Some(v) = cursor.next().await {
+                let t = v?;
+                result.push(CompressedEvent {
+                    title: t.event.app.clone(),
+                    time: t.timing,
+                    data: Box::new(t.event),
+                })
+            }
+
+            Ok(result)
+        })
+    }
+}
+
+#[derive(Deserialize, Serialize)]
+struct Notification {
+    app: String,
+    title: String,
+    content: String,
+}
+
+#[get("/notification/<password>/<app>/<title>/<content>")]
+async fn new_notification(
+    password: &str,
+    app: &str,
+    title: &str,
+    content: &str,
+    config: &State<Config>,
+    database: &State<Arc<Database>>,
+) -> (Status, Json<APIResult<()>>) {
+    if password != config.password {
+        return (Status::Unauthorized, Json(Err(APIError::AuthenticationError)));
+    }
+
+    match database
+        .register_single_event(&Event {
+            timing: Timing::Instant(Utc::now()),
+            id: Utc::now().timestamp_millis().to_string(),
+            plugin: AvailablePlugins::timeline_plugin_notification,
+            event: Notification {
+                app: app.to_string(),
+                title: title.to_string(),
+                content: content.to_string()
+            },
+        })
+        .await
+    {
+        Ok(_) => (Status::Ok, Json(Ok(()))),
+        Err(e) => (Status::InternalServerError, Json(Err(e.into()))),
+    }
+}
+
+#[get("/icon/<app>")]
+pub async fn app_icon(app: &str) -> Option<NamedFile> {
+    let mut path = PathBuf::from("../plugins/timeline_plugin_notification/icons/");
+    path.push(app);
+    NamedFile::open(path).await.ok()
+}
