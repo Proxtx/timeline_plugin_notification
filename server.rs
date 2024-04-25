00310e65f10857db52859c6ef41cd6dc1d7d5fc9
Proxtx <39201896+Proxtx@users.noreply.github.com>
2024-04-26 00:12:55 +0200

    some fixes and use the new error system

--- server.rs
+++ server.rs
@@ -115,7 +115,7 @@ async fn new_notification(
     {
         Ok(_) => (Status::Ok, Json(Ok(()))),
         Err(e) => {
-            crate::error::error(database.inner().clone(), &e, Some(<Plugin as crate::Plugin>::get_type()));
+            crate::error::error(database.inner().clone(), &e, Some(<Plugin as crate::Plugin>::get_type()), &config.error_report_url);
             (Status::InternalServerError, Json(Err(e.into())))
         },
     }
